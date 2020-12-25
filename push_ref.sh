420b5568dd13c523f978cabbaf8c6e695ceaf378
Atul <atul@Atuls-MacBook-Pro.local>
2020-12-26 02:04:42 +0530

    command updated

--- push_ref.sh
+++ push_ref.sh
@@ -1,4 +1,6 @@
 # FOR REFERENCE ONLY
 
+# python3 -m pip install --user --upgrade setuptools whe
+# python3 setup.py sdist bdist_wheel
 # pip install twine && python -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
-# python -m twine upload  dist/*                        
+# python -m twine upload  dist/*
