5aac830aef03737bdb348689a1e5a490e4b534bd
Atul <atul@Atuls-MacBook-Pro.local>
2021-03-20 23:59:59 +0530

    refactor

--- push_ref.sh
+++ push_ref.sh
@@ -1,7 +1,7 @@
 # FOR REFERENCE ONLY
 
 # python3 -m pip install --user --upgrade setuptools wheel twine
-# python3 setup.py sdist bdist_wheel
 # rm -rf bdist build pydotmap.egg-info dist
+# python3 setup.py sdist bdist_wheel
 # python3 -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
 # python3 -m twine upload  dist/*
